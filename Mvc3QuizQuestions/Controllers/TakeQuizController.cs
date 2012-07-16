8f0a8c124dd330b15f2743912d7fc479a0368c71
Jorge Barnaby <yorch@graphium.net>
2012-07-16 01:25:01 -0400

    TakeQuiz added.

---
+++ Mvc3QuizQuestions/Controllers/TakeQuizController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc3QuizQuestions.Models;
+using Mvc3QuizQuestions.ViewModels;
+
+namespace Mvc3QuizQuestions.Controllers
+{
+    public class TakeQuizController : BaseController
+    {
+        //
+        // GET: /TakeQuiz/1
+
+        public ViewResult Index(int id)
+        {
+            var viewModel = db.Quizs
+                .Where(q => q.Id == id)
+                .Include(q => q.Questions
+                    .Select(qe => qe.Choices))
+                .FirstOrDefault();
+            return View(viewModel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
