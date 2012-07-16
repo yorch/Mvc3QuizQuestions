592699ee05909a31d4816ee830e450a5aacd1fe7
Jorge Barnaby <yorch@graphium.net>
2012-07-16 01:24:40 -0400

    HtmlHelperExtesion added (BeginFormWithClassName).

---
+++ Mvc3QuizQuestions/Helpers/HtmlHelperExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc.Html;
+using System.Web.Mvc;
+
+namespace Mvc3QuizQuestions.Helpers
+{
+    public static class HtmlHelperExtension
+    {
+        public static MvcForm BeginFormWithClassName(this HtmlHelper helper, string cssClassName)
+        {
+            string controllerName = (string)helper.ViewContext.RouteData.Values["controller"];
+            string actionName = (string)helper.ViewContext.RouteData.Values["action"];
+            return helper.BeginForm(actionName, controllerName, FormMethod.Post, new { @class = cssClassName });
+        }
+    }
+}
