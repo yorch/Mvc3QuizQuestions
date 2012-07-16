d730d95f3d3e7afe0c9f59508051317a25069249
Jorge Barnaby <yorch@graphium.net>
2012-07-16 00:56:48 -0400

    QuestionController and views added.

---
+++ Mvc3QuizQuestions/Controllers/QuestionController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc3QuizQuestions.Models;
+
+namespace Mvc3QuizQuestions.Controllers
+{
+    public class QuestionController : BaseController
+    {
+        //
+        // GET: /Question/
+
+        public ViewResult Index(int quizId)
+        {
+            db.Quizs.Find(quizId);
+            return View(db.Questions.ToList());
+        }
+
+        //
+        // GET: /Question/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Question question = db.Questions.Find(id);
+            return View(question);
+        }
+
+        //
+        // GET: /Question/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Question/Create
+
+        [HttpPost]
+        public ActionResult Create(Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Questions.Add(question);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(question);
+        }
+
+        //
+        // GET: /Question/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            Question question = db.Questions.Find(id);
+            return View(question);
+        }
+
+        //
+        // POST: /Question/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(question).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(question);
+        }
+
+        //
+        // GET: /Question/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            Question question = db.Questions.Find(id);
+            return View(question);
+        }
+
+        //
+        // POST: /Question/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Question question = db.Questions.Find(id);
+            db.Questions.Remove(question);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
