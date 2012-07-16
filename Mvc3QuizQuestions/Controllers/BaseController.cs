8fc61fc25c8f69979d446ad0b90542350cf351ee
Jorge Barnaby <yorch@graphium.net>
2012-07-16 00:54:05 -0400

    EntityContext created.

---
+++ Mvc3QuizQuestions/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc3QuizQuestions.Models;
+using Mvc3QuizQuestions.DataAccess;
+
+namespace Mvc3QuizQuestions.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected EntityContext db = new EntityContext();
+    }
+}
---
+++ Mvc3QuizQuestions/DataAccess/EntityContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using Mvc3QuizQuestions.Models;
+
+namespace Mvc3QuizQuestions.DataAccess
+{
+    public class EntityContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, add the following
+        // code to the Application_Start method in your Global.asax file.
+        // Note: this will destroy and re-create your database with every model change.
+        // 
+        // System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<Mvc3QuizQuestions.Models.Mvc3QuizQuestionsContext>());
+
+        public DbSet<Quiz> Quizs { get; set; }
+        //public DbSet<Quiz> Quizzes { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<QuestionChoice> QuestionChoices { get; set; }
+        public DbSet<QuizAnswer> QuizAnswers { get; set; }
+        public DbSet<QuestionAnswer> QuestionAnswers { get; set; }
+
+        public override int SaveChanges()
+        {
+            DateTime now = DateTime.Now;
+            foreach (var entity in ChangeTracker.Entries<BaseEntity>()
+                                                .Where(e => e.State == EntityState.Added)
+                                                .Select(e => e.Entity))
+            {
+                entity.CreatedOn = now;
+            }
+
+            foreach (var entity in ChangeTracker.Entries<BaseEntity>()
+                                                .Where(e => e.State == EntityState.Modified)
+                                                .Select(e => e.Entity))
+            {
+                entity.UpdatedOn = now;
+            }
+
+            return base.SaveChanges();
+        }
+    }
+}
