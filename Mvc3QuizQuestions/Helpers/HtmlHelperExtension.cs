d47bcfb60a9bc1508390e77ff4493b57b7573ddc
Jorge Barnaby <yorch@graphium.net>
2012-07-16 20:46:50 -0400

    LabelFor methods added to HtmlHelperExtension.

--- Mvc3QuizQuestions/Helpers/HtmlHelperExtension.cs
+++ Mvc3QuizQuestions/Helpers/HtmlHelperExtension.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 using System.Web.Mvc.Html;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace Mvc3QuizQuestions.Helpers
 {
@@ -15,5 +17,28 @@ namespace Mvc3QuizQuestions.Helpers
             string actionName = (string)helper.ViewContext.RouteData.Values["action"];
             return helper.BeginForm(actionName, controllerName, FormMethod.Post, new { @class = cssClassName });
         }
+
+        public static MvcHtmlString LabelFor<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression, object htmlAttributes)
+        {
+            return LabelFor(html, expression, new RouteValueDictionary(htmlAttributes));
+        }
+
+        public static MvcHtmlString LabelFor<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression, IDictionary<string, object> htmlAttributes)
+        {
+            ModelMetadata metadata = ModelMetadata.FromLambdaExpression(expression, html.ViewData);
+            string htmlFieldName = ExpressionHelper.GetExpressionText(expression);
+            string labelText = metadata.DisplayName ?? metadata.PropertyName ?? htmlFieldName.Split('.').Last();
+            if (String.IsNullOrEmpty(labelText))
+            {
+                return MvcHtmlString.Empty;
+            }
+
+            TagBuilder tag = new TagBuilder("label");
+            tag.MergeAttributes(htmlAttributes);
+            tag.Attributes.Add("for", html.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldId(htmlFieldName));
+            tag.SetInnerText(labelText);
+            return MvcHtmlString.Create(tag.ToString(TagRenderMode.Normal));
+        }
+
     }
 }
