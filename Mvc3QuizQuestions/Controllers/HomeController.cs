97b4dae6e91712ac75803383e7fadfe04162e0a1
Jorge Barnaby <yorch@graphium.net>
2012-07-17 00:11:03 -0400

    TopScores created.

--- Mvc3QuizQuestions/Controllers/HomeController.cs
+++ Mvc3QuizQuestions/Controllers/HomeController.cs
@@ -19,5 +19,22 @@ namespace Mvc3QuizQuestions.Controllers
         {
             return View();
         }
+
+        public ActionResult TopScores()
+        {
+            int numberOfScores = 10;
+
+            var viewModel = db.AnswerQuizs
+                .OrderByDescending(a => a.Score)
+                .Take(numberOfScores);
+
+            return View(viewModel);
+            /*var viewModel = from a in db.AnswerQuizs
+                            orderby a.Score descending
+                            select a;*/
+            /*return View(viewModel
+                .Select(a => a.Questions.Select(q => q.Answer))
+                .Take(numberOfScores));*/
+        }
     }
 }
