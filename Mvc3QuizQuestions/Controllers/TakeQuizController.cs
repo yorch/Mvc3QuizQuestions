8c2e3a62d59eef92cd312c4a1c19a1d300f2801f
Jorge Barnaby <yorch@graphium.net>
2012-07-17 00:12:04 -0400

    TakeQuiz fixed and improved.

--- Mvc3QuizQuestions/Controllers/TakeQuizController.cs
+++ Mvc3QuizQuestions/Controllers/TakeQuizController.cs
@@ -22,6 +22,49 @@ namespace Mvc3QuizQuestions.Controllers
                 .Include(q => q.Questions
                     .Select(qe => qe.Choices))
                 .FirstOrDefault();
+
+            AnswerQuiz answerQuiz = new AnswerQuiz()
+            {
+                Quiz = viewModel,
+                User = User.Identity.Name,
+                Questions = new List<AnswerQuestion>()
+            };
+            foreach (var question in viewModel.Questions)
+            {
+                answerQuiz.Questions.Add(new AnswerQuestion()
+                {
+                    Question = question
+                });
+            }
+            return View(answerQuiz);
+        }
+
+        [HttpPost]
+        public ActionResult Process(AnswerQuiz answerQuiz)
+        {
+            answerQuiz.AnsweredOn = DateTime.Now;
+            answerQuiz.User = User.Identity.Name;
+            answerQuiz.Quiz = db.Quizs.Find(answerQuiz.Quiz.Id);
+            foreach (var question in answerQuiz.Questions)
+            {
+                question.Question = db.Questions.Find(question.Question.Id);
+                question.Answer = db.QuestionChoices.Find(question.Answer.Id);
+            }
+            answerQuiz.CalculateScore();
+
+            // Save it to DB
+            db.AnswerQuizs.Add(answerQuiz);
+            db.SaveChanges();
+
+            return RedirectToAction("Result", new { id = answerQuiz.Id });
+        }
+
+        public ViewResult Result(int id)
+        {
+            var viewModel = db.AnswerQuizs
+                .Where(a => a.Id == id)
+                .Include(a => a.Questions.Select(q => q.Answer))
+                .FirstOrDefault();
             return View(viewModel);
         }
 
