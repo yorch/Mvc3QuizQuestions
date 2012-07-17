1a17a17965ef5db60e1b4af7d087e2d1def05e63
Jorge Barnaby <yorch@graphium.net>
2012-07-16 20:46:10 -0400

    TakeQuiz JS added.

---
+++ Mvc3QuizQuestions/Scripts/take-quiz.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+    $('.question-choices').hide();
+
+    $('.question-title').css("cursor", "pointer").click(function () {
+        $(this).next('.question-choices').slideToggle(250);
+    });
+});
