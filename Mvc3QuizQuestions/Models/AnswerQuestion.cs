c7a59888faaf898f72c78292cf5c74558dba4f5c
Jorge Barnaby <yorch@graphium.net>
2012-07-17 00:06:39 -0400

    QuestionAnswer and QuestionChoice renamed as AnswerQuestion and AnswerChoice.

---
+++ Mvc3QuizQuestions/Models/AnswerQuestion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mvc3QuizQuestions.Models
+{
+    public class AnswerQuestion : BaseEntity
+    {
+        [Required]
+        public virtual AnswerQuiz QuizAnswer { get; set; }
+
+        public virtual Question Question { get; set; }
+
+        public virtual QuestionChoice Answer { get; set; }
+
+
+        public bool AnswerIsCorrect
+        {
+            get
+            {
+                return Answer.IsCorrect;
+            }
+        }
+    }
+}
---
+++ Mvc3QuizQuestions/Models/AnswerQuiz.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mvc3QuizQuestions.Models
+{
+    public class AnswerQuiz : BaseEntity
+    {
+        [Required]
+        public virtual Quiz Quiz { get; set; }
+
+        [Required]
+        public string User { get; set; }
+
+        [Required]
+        public DateTime AnsweredOn { get; set; }
+
+        [Required]
+        public int Score { get; set; }
+
+        public virtual ICollection<AnswerQuestion> Questions { get; set; }
+
+
+        public void CalculateScore()
+        {
+            int score = 0;
+            foreach (var answerQuestion in Questions)
+            {
+                if (answerQuestion.Answer.IsCorrect)
+                {
+                    score++;
+                }
+            }
+            Score = score;
+        }
+
+        public IList<Question> IncorrectQuestions
+        {
+            get
+            {
+                IList<Question> res = new List<Question>();
+                foreach (var question in Questions.ToList())
+                {
+                    if (!question.AnswerIsCorrect)
+                    {
+                        res.Add(question.Question);
+                    }
+                }
+                return res;
+            }
+        }
+    }
+}
